9ac18ffbdd0173eafe1732c056131d6a620c8de9
YiranWan <wanyiran_0223@163.com>
2020-12-22 16:27:36 +0800

    Initial Commit

---
+++ iDine/iDineApp.swift
@@ -0,0 +1,17 @@
+//
+//  iDineApp.swift
+//  iDine
+//
+//  Created by 万亦然 on 2020/12/22.
+//
+
+import SwiftUI
+
+@main
+struct iDineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
